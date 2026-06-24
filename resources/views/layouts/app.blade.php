d584594d11421e32f123968fcdfbde8cd5067f30
saurbhcoder <saurbhcoder@gmail.com>
2026-06-25 00:02:55 +0530

    x

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ asset('public/build/assets/app-DatMu1Dn.css') }}">
+        <script type="module" src="{{ asset('public/build/assets/app-BfpX1doZ.js') }}"></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
