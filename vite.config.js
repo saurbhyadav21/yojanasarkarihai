3c9fca29c2a0e68b80664b6abbca1dcfdba64b38
saurbhcoder <saurbhcoder@gmail.com>
2026-06-24 23:49:49 +0530

    x

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,3 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
 export default defineConfig({
     plugins: [
         laravel({
@@ -8,4 +5,5 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    base: '/public/',
 });
