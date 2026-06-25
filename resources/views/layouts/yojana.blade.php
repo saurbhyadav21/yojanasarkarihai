71d12dda77030ca56c3293923d84914be221f04f
saurbhcoder <saurbhcoder@gmail.com>
2026-06-25 09:41:20 +0530

    x

--- resources/views/layouts/yojana.blade.php
+++ resources/views/layouts/yojana.blade.php
@@ -163,9 +163,67 @@
 
 <body>
 
-    
+
 </body>
 
+    <!-- Header -->
+
+    <nav class="navbar navbar-expand-lg sticky-top">
+
+        <div class="container">
+
+            <a href="#" class="navbar-brand">
+                Yojana Sarkari Hai
+            </a>
+
+            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#menu">
+
+                <span class="navbar-toggler-icon"></span>
+
+            </button>
+
+
+            <div class="collapse navbar-collapse" id="menu">
+
+                <ul class="navbar-nav ms-auto align-items-lg-center">
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Schemes</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Tools</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Documents</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="">State Wise</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="">News</a>
+                    </li>
+
+                    <li class="nav-item ms-lg-3">
+                        <button class="scheme-btn">
+                            Scheme Finder
+                        </button>
+                    </li>
+
+                </ul>
+
+            </div>
+
+        </div>
+
+    </nav>
 <!-- ======================================
 PREMIUM FOOTER
 ====================================== -->
