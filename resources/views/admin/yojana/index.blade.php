4b66fdd4e2cde8d53f9eb9c73a71d1c63225d69a
saurbhcoder <saurbhcoder@gmail.com>
2026-06-25 13:52:41 +0530

    x

--- resources/views/admin/yojana/index.blade.php
+++ resources/views/admin/yojana/index.blade.php
@@ -5,8 +5,7 @@
         </h2>
     </x-slot>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 
     <div class="py-12">
